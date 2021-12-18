9cefc275808f79082cf079be985f08d20462a117
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-12-18 23:50:32 +0000

    Add the Veritcal search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    // Horizontal search
+    let horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
-}
+    // Veritcal search
+    for (let i = 0; i < letters[0].length; i++) {
+        let verticalLine = "";
+        for (let j = 0; j < letters.length; j++) {
+            verticalLine = verticalLine.concat(letters[j][i]);
+        }
+        if (verticalLine.includes(word)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
